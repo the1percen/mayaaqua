25a882338f4771b68c4e38714322c8d914bf4772
Miracle <mohanrajlenova@gmail.com>
2026-06-09 13:37:34 +0530

    vercel deploy

--- mayaaqua_proj/wsgi.py
+++ mayaaqua_proj/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mayaaqua_proj.settings")
 
 application = get_wsgi_application()
+app = application
