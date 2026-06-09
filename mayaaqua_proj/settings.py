8908844cb33adfb41eb15c7ab90b13b2f211446d
Miracle <mohanrajlenova@gmail.com>
2026-06-09 14:05:13 +0530

    Configure Supabase DB

--- mayaaqua_proj/settings.py
+++ mayaaqua_proj/settings.py
@@ -79,7 +79,9 @@ WSGI_APPLICATION = "mayaaqua_proj.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR}/db.sqlite3')
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR}/db.sqlite3')
+    )
 }
 
 
